93b022f1fd5e6cd947dbc954dad109e38781c40d
rana135 <rana286090@gmail.com>
2023-01-30 12:49:15 +0600

    vercel deploy build added

--- index.js
+++ index.js
@@ -144,6 +144,12 @@ async function run() {
             const result = await profileCollection.insertOne(order)
             res.send(result)
         })
+        app.delete("/deleteUser/:id", async(req, res)=>{
+            const id = req.params.id
+            const query = {_id:ObjectId(id)}
+            const result = await userCollection.deleteOne(query)
+            res.send(result)
+        })
     }
     finally {
         // await client.close();
