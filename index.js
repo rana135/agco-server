da15ac4112c15111a58ee44763b253cfe87d16ba
rana135 <rana286090@gmail.com>
2023-03-11 02:17:58 +0600

    jwt issue solve

--- index.js
+++ index.js
@@ -162,7 +162,7 @@ async function run() {
             }
         })
         // check user role in database:-
-        app.get('/admin/:email', async (req, res) => {
+        app.get('/admin/:email',verifyJWT, async (req, res) => {
             const email = req.params.email;
             const user = await userCollection.findOne({ email: email });
             const isAdmin = user?.role === 'admin';
